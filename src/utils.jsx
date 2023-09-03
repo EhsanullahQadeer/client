cbcdd9c1cd4454b84cb40788c090f8bd7aa89458
Ehsanullah Qadeer <108793461+EhsanullahQadeer@users.noreply.github.com>
2023-09-03 05:03:56 -0700

    url changes

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,9 +1,13 @@
+ let BACK_END_URL="http://localhost:5000/api/v1/"
 //  let BACK_END_URL="https://localhost/api/v1/"
- let BACK_END_URL="https://howsquare.com/api/v1"
+//  let BACK_END_URL="https://howsquare.com/api/v1"
 
+export let Story_Files_URL="http://localhost:5000/uploads/stories"
 // export let Story_Files_URL="https://localhost/uploads/stories"
-export let Story_Files_URL="https://howsquare.com/uploads/stories"
+// export let Story_Files_URL="https://howsquare.com/uploads/stories"
+
+export let Writer_Files_URL="http://localhost:5000/uploads/writer/images/"
 // export let Writer_Files_URL="https://localhost/uploads/writer/images/"
-export let Writer_Files_URL="https://howsquare.com/uploads/writer/images/"
+// export let Writer_Files_URL="https://howsquare.com/uploads/writer/images/"
 
 export {BACK_END_URL}
